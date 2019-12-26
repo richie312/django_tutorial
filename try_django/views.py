c7032014f69641c1979ccb353f866684f15c18dc
Aritra Chatterjee <richie.chatterjee31@gmail.com>
2019-12-26 20:22:10 +0530

    added the changes in views.py

--- try_django/views.py
+++ try_django/views.py
@@ -7,14 +7,14 @@ def home_page(request):
 	my_msg = "Hello there..."
 	if request.user.is_authenticated:
 		a = datetime.now()    
-		if a.hour > 12 & a.hour <=16:
+		if a.hour > 12 and a.hour <=16:
 		    greet = "Afternoon!"
 		    time = a.hour
-		elif a.hour > 1 & a.hour <=12:
+		elif a.hour > 1 and a.hour <=12:
 		    greet = "Morning!"
 		    time = a.hour
-		elif a.hour > 16 & a.hour <= 24:
-		    greet = "Evening"
+		elif a.hour > 16 and a.hour <= 24:
+		    greet = "Evening!"
 		    time = a.hour
 		context = {"message": my_msg,"greet": greet,"time":time}	
 	else:
